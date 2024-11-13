2895fee83ce4776627215217f96be47fcee65602
Rupeshpharande <pharanderupesh@gmail.com>
2024-11-14 01:20:43 +0530

    Add project files.

---
+++ Static/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1=new Student("Rupesh");
+            Console.WriteLine(s1.Print());
+            Console.WriteLine(Student.StudCount());
+
+        }
+    }
+}
---
+++ Static/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Student
+    {
+        private string name;
+        private int Rollno;
+        private static int count;
+
+
+        static Student()
+        {
+             count = 0;
+        }
+        public Student(string name)
+        {
+            count++;
+            Rollno = count;
+            this.name = name;
+        }
+        public string Print()
+        {
+            return $"{Rollno},{name}";
+        }
+        public static int StudCount()
+        {
+            return count;
+        }
+    }
+}
